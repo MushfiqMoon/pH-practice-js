7ab978a3cabe098e6c57d0dd8ec18545b30f8ee5
MushfiqMoon <mushfiq.moon013@google.com>
2022-08-08 21:24:47 +0600

    complete the moduld 17 practice

--- custom.js
+++ custom.js
@@ -88,6 +88,40 @@ getGread = () => {
 }
 
 
+// Module 17 : problem 3
+
+getMaxNum = () => {
+    let num1 = parseInt(document.getElementById('number1').value);
+    let num2 = parseInt(document.getElementById('number2').value);
+    let num3 = parseInt(document.getElementById('number3').value);
+
+
+    // calculation
+    // let bigNum = Math.max(num1, num2, num3)
+
+    let bigNum = 0
+
+    //practice problem 17.1
+    if (num1 > num2 && num1 > num3) {
+        let bigNum = num1
+    } else if (num2 > num1 && num1 > num3) {
+        let bigNum = num2
+    }
+    else {
+        let bigNum = num3
+    }
+    //practice problem 17.2
+    if (num1 === num2 || num1 === num3 || num2 === num3) {
+        document.getElementById("isosceles").innerHTML = `Your Triangle is Isosceles => two sides are equal`
+    } else {
+        document.getElementById("isosceles").innerHTML = `Your Triangle is <span class='text-danger'>Not</span> Isosceles => two sides are not equal`
+    }
+    // result
+    document.getElementById("bigNum").innerHTML = `Your Largest Number is :- ${bigNum}`
+}
+
+
+
 // Module 19 : problem 4
 
 calculateSquare = () => {
